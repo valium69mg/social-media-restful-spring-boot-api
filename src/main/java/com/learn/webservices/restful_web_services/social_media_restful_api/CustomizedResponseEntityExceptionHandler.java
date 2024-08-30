d3196b6ae53ccea2f1d08614f4518206aa2522a2
valium69mg <xdeus.v2@gmail.com>
2024-08-30 16:59:15 -0600

    Add a response error for argument not valid on post request

--- src/main/java/com/learn/webservices/restful_web_services/social_media_restful_api/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/learn/webservices/restful_web_services/social_media_restful_api/CustomizedResponseEntityExceptionHandler.java
@@ -3,8 +3,11 @@ package com.learn.webservices.restful_web_services.social_media_restful_api;
 import java.time.LocalDate;
 import java.util.NoSuchElementException;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -12,16 +15,31 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
-	
-	@ExceptionHandler(value= {Exception.class})
-	public final ResponseEntity<SocialMediaErrorDetails> handleAllException(Exception ex, WebRequest request) throws Exception {
-		SocialMediaErrorDetails errorDetails = new SocialMediaErrorDetails(LocalDate.now(),ex.getMessage(),request.getDescription(false));
+
+	@ExceptionHandler(value = { Exception.class })
+	public final ResponseEntity<SocialMediaErrorDetails> handleAllException(Exception ex, WebRequest request)
+			throws Exception {
+		SocialMediaErrorDetails errorDetails = new SocialMediaErrorDetails(LocalDate.now(), ex.getMessage(),
+				request.getDescription(false));
 		return new ResponseEntity<SocialMediaErrorDetails>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
-	
-	@ExceptionHandler(value= {NoSuchElementException.class})
-	public final ResponseEntity<SocialMediaErrorDetails> handleNoSuchElementExceptionException(Exception ex, WebRequest request) throws Exception {
-		SocialMediaErrorDetails errorDetails = new SocialMediaErrorDetails(LocalDate.now(),ex.getMessage(),request.getDescription(false));
+
+	@ExceptionHandler(value = { NoSuchElementException.class })
+	public final ResponseEntity<SocialMediaErrorDetails> handleNoSuchElementExceptionException(Exception ex,
+			WebRequest request) throws Exception {
+		SocialMediaErrorDetails errorDetails = new SocialMediaErrorDetails(LocalDate.now(), ex.getMessage(),
+				request.getDescription(false));
 		return new ResponseEntity<SocialMediaErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
 	}
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+		SocialMediaErrorDetails errorDetails = new SocialMediaErrorDetails(LocalDate.now(), ex.getMessage(),
+				request.getDescription(false));
+
+		return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+	}
+
 }
